7419e8417255c0a1967636c3c08826c1c5121f27
DriesGeysens <dries.geysens@intodata.eu>
2023-04-25 08:43:32 +0200

    Added Lesson 6

---
+++ 06 Scheduling/popen.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.Popen(["C:\\Users\\geysedr\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", r'C:\Users\geysedr\Documents\Thomas More\Scripting\scripts\06 Scheduling\helloworld.py'])
